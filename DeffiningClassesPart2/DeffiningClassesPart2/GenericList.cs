b14a8ec1fca315ae6b8c4d826d87b1808b41282d
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-06-17 09:44:36 +0300

    I added one class for path storage.

---
+++ DeffiningClassesPart2/DeffiningClassesPart2/GenericList.cs
@@ -0,0 +1,70 @@
+﻿namespace DeffiningClassesPart2
+{
+    using System.Collections.Generic;
+
+    class GenericList<T> 
+    {
+        // fields
+
+        private List<T> elements;
+        private int index = 0;
+
+        // constructures
+
+        public GenericList(int capacity)
+        {
+            this.elements = new List<T>();
+        }
+
+        // properties
+
+        public List<T> Elements
+        {
+            get { return this.elements; }
+            set { this.elements = value; }
+        }
+
+        // methods
+
+        public void Add (T element)
+        {
+            this.Elements[this.index] = element;
+            this.index++;
+        }
+
+        public T AccessEl (int index)
+        {
+            return this.Elements[index];
+        }
+
+        public void RemoveEl (int index)
+        {
+            //this.Elements
+        }
+
+        public void InsertEl (int index, T element)
+        {
+            this.Elements[index] = element;
+        }
+
+        public void Clear ()
+        {   
+            this.Elements.Clear();
+        }
+
+        public T Min ()
+        {
+            T min = this.Elements[0];
+            
+            for (int i = 0; i < this.Elements.Count - 1; i++)
+            {
+                if (min > this.Elements[i + 1])
+                {
+
+                }
+            }
+
+            return min;
+        }
+    }
+}
--- DeffiningClassesPart2/DeffiningClassesPart2/PathStorage.cs
+++ DeffiningClassesPart2/DeffiningClassesPart2/PathStorage.cs
@@ -1,7 +1,32 @@
 ﻿namespace DeffiningClassesPart2
 {
-    public class PathStorage
-    {
+    using System.Collections.Generic;
 
+    public static class PathStorage
+    {
+        private static List<string> savePaths;
+
+        static PathStorage ()
+        {
+            SavePaths = new List<string>();
+        }
+
+        public static List<string> SavePaths
+        {
+            get
+            {
+                return savePaths;
+            }
+
+            set
+            {
+                savePaths = value;
+            }
+        }
+
+        public static void Save()
+        {
+
+        }
     }
 }
