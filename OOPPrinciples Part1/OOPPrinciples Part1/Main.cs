3f1e10cd392bc7995b23cbd85a617fa0d98eff46
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-06-23 23:24:03 +0300

    Add a new homework

---
+++ OOPPrinciples Part1/OOPPrinciples Part1/Main.cs
@@ -0,0 +1,16 @@
+﻿namespace OOPPrinciples_Part1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class School
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
