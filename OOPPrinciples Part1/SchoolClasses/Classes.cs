52c1aa0885754895e04c44db3c750e1c713eff82
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-06-23 23:30:10 +0300

    Changed

---
+++ OOPPrinciples Part1/SchoolClasses/Classes.cs
@@ -0,0 +1,36 @@
+﻿namespace OOPPrinciples_Part1
+{
+    public class Classes 
+    {
+        private string teachers;
+        private string classType;
+
+        public string Teachers
+        {
+            get
+            {
+                return this.teachers;
+            }
+
+            set
+            {
+                this.teachers = value;
+            }
+        }
+
+        private string ClassType
+        {
+            get
+            {
+                return this.classType;
+            }
+            set
+            {
+                this.classType = value;
+            }
+
+        }
+
+
+    }
+}
