80f38bd2143296e001a4b7a35a9565b3cd2edbf1
Dimitar Stoqnov <stoyanov.dimi.tri@gmail.com>
2016-06-29 12:12:06 +0300

    Complete the homework.

--- OOPPrinciples Part2/02.BankAccounts/StartUp.cs
+++ OOPPrinciples Part2/02.BankAccounts/StartUp.cs
@@ -7,11 +7,27 @@
     using System.Threading.Tasks;
     using Bank;
 
-    public class StartUp
+    public class Startup
     {
         static void Main()
         {
+            var doncho = new Loan(CustomerType.Company, 2, 3, 3);
 
+            Console.WriteLine("------------- Doncho ------------");
+
+            Console.WriteLine(doncho.Calculate(doncho.InterestRate, doncho.Months));
+
+            var cuki = new Deposit(CustomerType.Individual, 23, 1001, 101);
+
+            Console.WriteLine("------------- Cuki ------------");
+
+            Console.WriteLine(cuki.Calculate(cuki.InterestRate, cuki.Months));
+
+            var evlogi = new Mortgage(CustomerType.Company, 12, 45, 6);
+
+            Console.WriteLine("------------- Evlogi ------------");
+
+            Console.WriteLine(evlogi.Calculate(evlogi.InterestRate, evlogi.Months));
         }
     }
 }
